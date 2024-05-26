6396e306826b805bbef7d61598dbdf53dc380205
Chandan Marle <chandanmarlex12@gmail.com>
2024-05-25 21:59:35 -0600

    Create movement for pacman

---
+++ Scripts/pacman.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+func _physics_process(delta):
+
+	# Handle jump.
+	if Input.is_action_just_pressed("move_up"):
+		velocity.x = 0
+		velocity.y = -SPEED
+	elif Input.is_action_just_pressed("move_down"):
+		velocity.x = 0
+		velocity.y = SPEED
+	elif Input.is_action_just_pressed("move_left"):
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_just_pressed("move_right"):
+		velocity.x = SPEED
+		velocity.y = 0
+		
+
+	move_and_slide()
