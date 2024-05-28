b471c84ad99740f48afb939e6cf68b07eef0c5f9
Donald Winkelman <dmwink.tx@gmail.com>
2024-05-27 22:29:54 -0500

    Debugging pac man movement

--- Scripts/pacman.gd
+++ Scripts/pacman.gd
@@ -1,24 +1,16 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var left_1 = $Rays/Left1
-@onready var right_1 = $Rays/Right1
-@onready var left_2 = $Rays/Left2
-@onready var left_3 = $Rays/Left3
-@onready var right_2 = $Rays/Right2
-@onready var right_3 = $Rays/Right3
-@onready var up_1 = $Rays/Up1
-@onready var up_3 = $Rays/Up3
-@onready var up_2 = $Rays/Up2
-@onready var down_1 = $Rays/Down1
-@onready var down_2 = $Rays/Down2
-@onready var down_3 = $Rays/Down3
+@onready var down = $"Turn Detection/Down"
+@onready var up = $"Turn Detection/Up"
+@onready var left = $"Turn Detection/Left"
+@onready var right = $"Turn Detection/Right"
 
-const SPEED = 100.0
+const SPEED = 40.0
 var current_direction = 'left'
 var next_direction = 'left'
 
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_just_pressed('move_left'):
 		next_direction = 'left'
 	if Input.is_action_just_pressed('move_right'):
@@ -29,32 +21,32 @@ func _process(delta):
 		next_direction = 'down'
 	match next_direction:
 		'left':
-			if not (left_1.is_colliding() or left_2.is_colliding() or left_3.is_colliding()):
+			if not left.is_colliding():
 				current_direction = next_direction
 		'right':
-			if not (right_1.is_colliding() or right_2.is_colliding() or right_3.is_colliding()):
+			if not right.is_colliding():
 				current_direction = next_direction
 		'up':
-			if not (up_1.is_colliding() or up_2.is_colliding() or up_3.is_colliding()):
+			if not up.is_colliding():
 				current_direction = next_direction
 		'down':
-			if not (down_1.is_colliding() or down_2.is_colliding() or down_3.is_colliding()):
+			if not down.is_colliding():
 				current_direction = next_direction
 	match current_direction:
 		'left':
 			velocity.x = -SPEED
 			velocity.y = 0
-			animated_sprite.play("Leftward")
+			animated_sprite.play('Leftward')
 		'right':
 			velocity.x = SPEED
 			velocity.y = 0
-			animated_sprite.play("Rightward")
+			animated_sprite.play('Rightward')
 		'up':
 			velocity.x = 0
 			velocity.y = -SPEED
-			animated_sprite.play("Upward")
+			animated_sprite.play('Upward')
 		'down':
 			velocity.x = 0
 			velocity.y = SPEED
-			animated_sprite.play("Downward")
+			animated_sprite.play('Downward')
 	move_and_slide()
