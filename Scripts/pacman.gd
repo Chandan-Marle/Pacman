656e5f9b3bb53252ff1b2026ec7343f68258860b
Donald Winkelman <dmwink.tx@gmail.com>
2024-05-27 19:31:40 -0500

    Very buggy next direction-knowing movement system created

--- Scripts/pacman.gd
+++ Scripts/pacman.gd
@@ -1,27 +1,57 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var left_1 = $Left1
+@onready var right_1 = $Right1
+@onready var left_2 = $Left2
+@onready var right_2 = $Right2
+@onready var up_1 = $Up1
+@onready var up_2 = $Up2
+@onready var down_1 = $Down1
+@onready var down_2 = $Down2
 
 const SPEED = 100.0
-
+var current_direction = 'left'
+var next_direction = 'left'
 
 func _physics_process(delta):
-
-	# Handle jump.
-	if Input.is_action_just_pressed("move_up"):
-		velocity.x = 0
-		velocity.y = -SPEED
-		animated_sprite.play("Upward")
-	elif Input.is_action_just_pressed("move_down"):
-		velocity.x = 0
-		velocity.y = SPEED
-		animated_sprite.play("Downward")
-	elif Input.is_action_just_pressed("move_left"):
-		velocity.x = -SPEED
-		velocity.y = 0
-		animated_sprite.play("Leftward")
-	elif Input.is_action_just_pressed("move_right"):
-		velocity.x = SPEED
-		velocity.y = 0
-		animated_sprite.play("Rightward")
+	print(current_direction, next_direction)
+	if Input.is_action_just_pressed('move_left'):
+		next_direction = 'left'
+	if Input.is_action_just_pressed('move_right'):
+		next_direction = 'right'
+	if Input.is_action_just_pressed('move_up'):
+		next_direction = 'up'
+	if Input.is_action_just_pressed('move_down'):
+		next_direction = 'down'
+	match next_direction:
+		'left':
+			if not (left_1.is_colliding() or left_2.is_colliding()):
+				current_direction = next_direction
+		'right':
+			if not (right_1.is_colliding() or right_2.is_colliding()):
+				current_direction = next_direction
+		'up':
+			if not (up_1.is_colliding() or up_2.is_colliding()):
+				current_direction = next_direction
+		'down':
+			if not (down_1.is_colliding() or down_2.is_colliding()):
+				current_direction = next_direction
+	match current_direction:
+		'left':
+			velocity.x = -SPEED
+			velocity.y = 0
+			animated_sprite.play("Leftward")
+		'right':
+			velocity.x = SPEED
+			velocity.y = 0
+			animated_sprite.play("Rightward")
+		'up':
+			velocity.x = 0
+			velocity.y = -SPEED
+			animated_sprite.play("Upward")
+		'down':
+			velocity.x = 0
+			velocity.y = SPEED
+			animated_sprite.play("Downward")
 	move_and_slide()
